5481968d9bb469a92d4be3525d3a5131b4e81d51
farizaiis <farizaiis@hotmail.com>
2021-12-10 15:15:38 +0700

    init project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const Router = require('./routes/index');
+const moment = require('moment');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', Router);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Server running',
+        serverTime: moment(new Date()).local(),
+    });
+});
+
+app.get('*', function (req, res) {
+    res.status(404).send('Not found');
+});
+
+module.exports = app;
